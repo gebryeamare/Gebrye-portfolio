872922bbe914608417f9272b476baa43a2efdf14
gebryeamare <gebryeamare5@gmail.com>
2026-08-13 16:30:40 +0300

    feat: add phone number to contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -14,6 +14,7 @@ import {
   MapPin,
   Send,
   AlertCircle,
+  PhoneCall,
 } from "lucide-react";
 
 import type { Icon } from "@/types";
@@ -144,6 +145,12 @@ export default function Contact() {
       icon: Mail,
       onCopy: copyEmail,
       copied,
+    },
+    {
+      label: "Phone",
+      value: "+251 918-763-378",
+      href: "tel:+251918763378",
+      icon: PhoneCall,
     },
     {
       label: "GitHub",
